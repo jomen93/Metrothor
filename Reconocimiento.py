6ec4017680faf08e774b0a48c5e748a8bb2a47f4
Johan Méndez <jsmendezh@unal.edu.co>
2019-04-01 22:17:02 -0500

    Update Reconocimiento.py

--- Reconocimiento.py
+++ Reconocimiento.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt 
 import tensorflow as tf
 from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
