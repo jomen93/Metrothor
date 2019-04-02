0193ffbd59337c1b73ddad5214e5f707afd4504a
Johan Méndez <jsmendezh@unal.edu.co>
2019-04-01 22:16:18 -0500

    Lectura imagenes
    Falta poner bien la dimensión del tensor para poder hacer el fit

--- Almacen/Catalogo/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from Catalogo.models import diametro, TipoMaterial, Tornillo,Existencia
-
-admin.site.register(Tornillo)
-admin.site.register(diametro)
-admin.site.register(TipoMaterial)
-admin.site.register(Existencia)
-
