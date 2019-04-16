7d803dbd119d0523c04dc846cdd11ac1fba045e2
Johan Méndez <jsmendezh@unal.edu.co>
2019-04-16 17:45:39 -0500

    prueba

--- Reconocimiento.py
+++ Reconocimiento.py
@@ -8,12 +8,17 @@ img_size = 50
 
 imP = [cv2.imread(file,cv2.IMREAD_GRAYSCALE) for file in glob.glob("imagenes/mascota/Perro100/*.jpg")]
 imG = [cv2.imread(file,cv2.IMREAD_GRAYSCALE) for file in glob.glob("imagenes/mascota/Gato100/*.jpg")]
-#yimP = np.zeros(imP)
-#imG = np.ones(imG)
+
+yimP = np.zeros(len(imP))
+yimG = np.ones(len(imG))
 
 
-#X_train, Ytrain = imP[:0.8*len(imP)],yimP[]
+X_train, Ytrain = np.array([imP[:int(0.8*len(imP))],yimP[:int(0.8*len(imP))]])
+X_test, Ytest   = np.array([imP[int(0.8*len(imP)):len(imP)],yimP[int(0.8*len(imP)):len(imP)]])
 
 
-plt.imshow(imG[10],cmap = "gray")
-plt.show()
+
+X_train = X_train.reshape(len(X_train),img_size,img_size,1)
+X_test = X_test.reshape(len(X_test),img_size,img_size,1)
+
+print ("Done!")
