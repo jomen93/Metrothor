36294ca4bed708ce8416658c908ae4ab0b26f3b0
Johan <jsmendezh@unal.edu.co>
2022-11-16 14:45:50 -0500

    Inicio proyecto dash

---
+++ tableros/tableros/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tableros project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tableros.settings')
+
+application = get_wsgi_application()
